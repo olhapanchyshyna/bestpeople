e56328dbc08cfe9d5638dcd2deeac6913fd1b286
Olya <olapancisina1@gmail.com>
2024-04-15 09:02:32 +0200

    adaptive not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,15 +6,17 @@ export default function notFound() {
     <section className="container py-[44px]">
       <H2 text="Page not found" />
       <div className="m-auto flex flex-col justify-center">
-        <div className="flex justify-center">
-          <div className="flex h-[300px] items-center text-[300px] text-gray-900">
+        <div className="my-[50px] flex justify-center">
+          <div className="flex h-[150px] items-center text-[150px] text-gray-900 sm:h-[200px] sm:text-[200px] lg:h-[300px] lg:text-[300px]">
             4
-            <span className="flex h-[300px] items-center text-[#FCA600]">
+            <span className="flex h-[150px] items-center  text-[#FCA600] sm:h-[200px]  lg:h-[300px]">
               0
             </span>
             4
           </div>
-          <div className="h-[50px] w-[50px] rotate-90 text-[40px] ">error</div>
+          <div className="h-[50px] w-[50px] rotate-90 text-[32px] lg:text-[40px] ">
+            error
+          </div>
         </div>
         <div className="text-center text-[16px] text-[#1A1A1A]">
           Page not found.
@@ -23,7 +25,7 @@ export default function notFound() {
           It may have been moved, or you simply entered the page address
           incorrectly.
         </div>
-        <Button text="Home Page" href='/' className="m-auto mt-[45px]" />
+        <Button text="Home Page" href="/" className="m-auto mt-[45px]" />
       </div>
     </section>
   );
