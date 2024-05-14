f56314f8115e1525a0838ca39c024e37c0909b16
Olya <olapancisina1@gmail.com>
2024-05-14 13:08:09 +0200

    add total count product in basket page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import NavigationMenuItems from "./navigation-menu-items";
 import PageIcons from "./page-icons";
-import Breadcrumbs from './breadcrumbs'
 
 export default function Header() {
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <header className="container flex h-[75px] items-center justify-between">
         <Link href="/" className="order-2 md:order-none">
           <Image src="/logo-color.svg" alt="logo" width={100} height={100} />
@@ -16,7 +15,6 @@ export default function Header() {
 
         <PageIcons />
       </header>
-			
     </div>
   );
 }
--- src/components/page-icons.tsx
+++ src/components/page-icons.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Phone from './phone'
+import { getServerSideArrayCookie } from '@/lib/cookies/server/get-server-side-array-cookie'
 
-export default function PageIcons() {
+export default async function PageIcons() {
+	const cookieGoodsArrays = await getServerSideArrayCookie("basket");
+
+	const totalQuantity = cookieGoodsArrays?.reduce((total, currentItem) => {
+    return total + currentItem.quantity;
+  }, 0);
+
 	return (
 		<div className='flex justify-between md:w-64 order-3 md:order-none'>
 			<Phone type='header' width={28} height={28} className='hidden md:flex'/>
@@ -10,8 +17,9 @@ export default function PageIcons() {
 				<Link href='/login'>
 					<Image src='/profile.svg' alt='profile' width={30} height={30} />
 				</Link>
-				<Link href='/basket'>
+				<Link href='/basket' className='relative'>
 					<Image src='/basket.svg' alt='basket' width={17} height={25} />
+					<div className='absolute text-[8px] bg-[#fca600] top-[-8px] right-[-8px] rounded-full px-[3px] py-[1px] text-white'>{totalQuantity}</div>
 				</Link>
 			</div>
 		</div>
