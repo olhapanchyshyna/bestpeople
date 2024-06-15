778ad183ea04ebaabffe28febd5d5a2e67dc927b
Olya <olapancisina1@gmail.com>
2024-06-15 10:14:09 +0200

    fix send to telegram route

--- src/app/api/send-to-telegram/route.ts
+++ src/app/api/send-to-telegram/route.ts
@@ -1,18 +1,12 @@
-// pages/api/send-to-telegram/route.ts
 import prisma from "@/lib/db";
-import type { NextApiRequest, NextApiResponse } from "next";
-import fetch from "node-fetch"; // Импортируем fetch для отправки запросов
+import { NextRequest, NextResponse } from 'next/server';
+import fetch from 'node-fetch'; // Импортируем fetch для отправки запросов
 
-export const postHandler = async (
-  req: NextApiRequest,
-  res: NextApiResponse,
-) => {
-  const { userId } = req.body;
-
-  console.log()
+export async function POST(req: NextRequest) {
+  const { userId } = await req.json();
 
   if (!userId) {
-    return res.status(400).json({ message: "Bad Request: userId is required" });
+    return NextResponse.json({ message: "Bad Request: userId is required" }, { status: 400 });
   }
 
   try {
@@ -21,7 +15,7 @@ export const postHandler = async (
     });
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
 
     // Получаем заказы пользователя или любые другие данные, которые хотите отправить
@@ -31,34 +25,28 @@ export const postHandler = async (
     const response = await fetch(
       `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           chat_id: process.env.TELEGRAM_CHAT_ID,
           text: `User ${user.name} has made an order:\n\n${JSON.stringify(orders, null, 2)}`,
         }),
-      },
+      }
     );
 
     if (!response.ok) {
-      throw new Error("Failed to send message to Telegram");
+      throw new Error('Failed to send message to Telegram');
     }
 
-    res.status(200).json({ message: "Data sent to Telegram successfully" });
+    return NextResponse.json({ message: "Data sent to Telegram successfully" });
   } catch (error) {
     console.error("Failed to send data to Telegram:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
   }
-};
+}
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    return postHandler(req, res);
-  } else {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
-};
-
-export default handler;
+export async function OPTIONS() {
+  return NextResponse.json({ message: "Method not allowed" }, { status: 405 });
+}
