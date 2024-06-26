beccbcdcdb8d35d807176f6638862f780dd19c1b
Olya <olapancisina1@gmail.com>
2024-06-26 13:44:50 +0200

    add AspectRatio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,23 @@ import Advantages from "@/components/advantages";
 import Benefits from "@/components/benefits";
 import CategorySlider from "@/components/category-slider";
 import PopularGoods from "@/components/popular-goods";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from "next/image";
 
 export default function page() {
   return (
     <main>
-      <section>
-        <picture>
-          <source
-            media="(max-width: 550px)"
-            srcSet="/bg-with-arms-mobile.png"
-            className=""
-          />
+      <section className="!h-[calc(100vh-75px)]">
+        <AspectRatio ratio={16 / 8}>
           <Image
+            fill={true}
             src="/bg-with-arms.png"
+            objectFit="cover"
+            objectPosition="center"
             alt="bg-with-arms"
-            width={1880}
-            height={750}
-            className="m-auto bg-no-repeat max-[550px]:h-[500px] max-[450px]:h-[430px]  xl:h-[80vh] xl:w-[100vw]"
+            className="!relative m-auto !h-[calc(100vh-75px)] bg-no-repeat xl:w-[100vw]"
           />
-        </picture>
+        </AspectRatio>
       </section>
       <CategorySlider />
       <PopularGoods />
